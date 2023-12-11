ed330aacd80d0556f31b08c7f79e8f11c55f3869
Henrik Pedersen <Henrik.Pedersen.vs@gmail.com>
2023-12-11 13:25:19 +0100

    Almost there, but not quite

--- AdventofCodeday1.cs
+++ AdventofCodeday1.cs
@@ -12,50 +12,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> Nums = new List<int>();
+        var numWords = new Dictionary<string, int>
+        {
+            {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4},
+            {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}
+        };
+        List<int> nums = new List<int>();
         int sum = 0;
 
-        string input;
-        string result ="";
         string[] lines = File.ReadAllLines("input.txt");
         char[] digitsArray = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-        for(int j=0; j<lines.Count();j++)
+        for (int j = 0; j < lines.Length; j++)
         {
-            input=lines[j];
+            string input = lines[j];
 
-        for (int i = 0; i < input.Length; i++)
-        {
-            if (digitsArray.Contains(input[i]))
+            for (int i = 0; i < input.Length; i++)
             {
-                int digit = int.Parse(input[i].ToString());
-                Nums.Add(digit);
+                if (digitsArray.Contains(input[i]))
+                {
+                    nums.Add(int.Parse(input[i].ToString()));
+                }
+
+                for (int end = i + 1; end <= input.Length; end++)
+                {
+                    string substring = input.Substring(i, end - i);
+                    if (numWords.ContainsKey(substring))
+                    {
+                        i = end - 1;
+                        break;
+                    }
+                }
             }
+
+            string result = "";
+            if (nums.Count > 1)
+            {
+                result = nums.FirstOrDefault() + nums.LastOrDefault().ToString();
+            }
+            else if (nums.Count == 1)
+            {
+                result = nums.First().ToString();
+            }
+
+            nums.Clear();
+
+            if (int.TryParse(result, out int parsedResult))
+            {
+                sum += parsedResult;
+            }
+
+            Console.WriteLine($"Iteration: {j + 1} Sum:{sum}");
         }
 
-        if(Nums.Count>1){
-        result = Nums.FirstOrDefault() + Nums.LastOrDefault().ToString();
-        }
-        else if(Nums.Count==1)
-        {
-        result = Nums.First().ToString();
-        }
-        else{
-        result = "";
-        }
-
-        Nums.Clear();
-
-        if (int.TryParse(result, out int parsedResult))
-        {
-            sum += parsedResult;
-        }
-
-            System.Console.WriteLine($"Iteration: {j+1} Sum:{sum}");
-
-        }   //End of j
-
-        System.Console.WriteLine($"{sum}");
-
+        Console.WriteLine($"Total Sum: {sum}");
     }
 }
