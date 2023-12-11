9c6b6a60407ec9eff1710d70dc9fa32949a8389e
Henrik Pedersen <Henrik.Pedersen.vs@gmail.com>
2023-12-11 11:05:43 +0100

    Commit

---
+++ AdventofCodeday1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Make list for holding nums
+        //Hook to input
+        //Conditions to check for
+        //Add to numlist
+        //SortNumlist
+        //ConvertShenanigans
+        //add int to sum
+
+        //repeat
+        
+
+    }
+}
